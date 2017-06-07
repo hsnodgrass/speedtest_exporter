6fc594a0885273f505f946ba962d1398290beaf8
Heston Snodgrass <heston.snodgrass@connexta.com>
2017-06-07 10:12:33 -0700

    Added error handling and logging for JSON parse.

--- speedtest_exporter.py
+++ speedtest_exporter.py
@@ -2,6 +2,8 @@
 
 import subprocess
 import json
+import sys
+from __future__ import print_function
 from prometheus_client import Gauge, start_http_server
 
 download_speed_gauge = Gauge('download_speed', 'Download speed in bits')
@@ -10,11 +12,21 @@ latency_speed_gauge = Gauge('latency_speed', 'Internet Latency')
 
 start_http_server(8001)
 
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
 def main():
     while True:
         s1 = subprocess.Popen(['speedtest-cli', '--json'], stdout=subprocess.PIPE, universal_newlines=True)
         s2 = s1.communicate()
-        st_json = json.loads(s2[0])
+        try:
+            st_json = json.loads(s2[0])
+        except JSONDecodeError as err:
+            eprint("ERROR: Failed to parse JSON, setting all values to 0!")
+            st_json['download'] = 0
+            st_json['upload'] = 0
+            st_json['ping'] = 0
+        
         download_speed_gauge.set(st_json['download'])
         upload_speed_gauge.set(st_json['upload'])
         latency_speed_gauge.set(st_json['ping'])
