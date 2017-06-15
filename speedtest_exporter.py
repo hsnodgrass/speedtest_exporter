06179452008a4dcfacbfb07eaa92facc18677ca2
Heston Snodgrass <heston.snodgrass@connexta.com>
2017-06-15 15:10:05 -0700

    Caught a case of the dumb with the last commit, fixed that dumbness.

--- speedtest_exporter.py
+++ speedtest_exporter.py
@@ -5,14 +5,15 @@ import json
 import sys
 from prometheus_client import Gauge, start_http_server
 
-download_speed_gauge = Gauge('speed_measured', 'The metric speed being exported.')
-upload_speed_gauge = Gauge('speed_measured', 'The metric speed being exported.')
-latency_speed_gauge = Gauge('speed_measured', 'The metric speed being exported.')
+download_speed_gauge = Gauge('download_speed', 'Download speed.')
+upload_speed_gauge = Gauge('upload_speed', 'Upload speed.')
+latency_speed_gauge = Gauge('latency_speed', 'Network latency.')
 
 start_http_server(8000)
 
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
+    print(*args, file='/var/log/ste_err.log', **kwargs)
 
 def main():
     while True:
@@ -24,10 +25,9 @@ def main():
             eprint("ERROR: Failed to parse JSON, setting all values to 0!")
             st_json = {'download': 0, 'upload': 0, 'ping': 0}
 
-        download_speed_gauge.labels(speed_measured='download').set(st_json['download'])
-        upload_speed_gauge.labels(speed_measured='upload').set(st_json['upload'])
-        latency_speed_gauge.labels(speed_measured='latency').set(st_json['ping'])
+        download_speed_gauge.set(st_json['download'])
+        upload_speed_gauge.set(st_json['upload'])
+        latency_speed_gauge.set(st_json['ping'])
 
 if __name__ == "__main__":
     main()
-    
