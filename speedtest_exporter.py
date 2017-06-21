bd88bc915f7da558fc8fe19a74c633d6ae06a6a3
Heston Snodgrass <heston.snodgrass@connexta.com>
2017-06-21 14:14:02 -0700

    Nested st_json variable assignment to remove possibility of it being referenced before assignment.

--- speedtest_exporter.py
+++ speedtest_exporter.py
@@ -11,20 +11,29 @@ latency_speed_gauge = Gauge('latency_speed', 'Network latency.')
 
 start_http_server(8000)
 
+
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
-    print(*args, file='/var/log/ste_err.log', **kwargs)
+
 
 def main():
     while True:
-        s1 = subprocess.Popen(['speedtest-cli', '--json'], stdout=subprocess.PIPE, universal_newlines=True)
-        s2 = s1.communicate()
         try:
-            st_json = json.loads(s2[0])
-        except json.decoder.JSONDecodeError:
-            eprint("ERROR: Failed to parse JSON, setting all values to 0!")
-            st_json = {'download': 0, 'upload': 0, 'ping': 0}
+            print('Running speedtest-cli subprocess.')
+            s1 = subprocess.Popen(['speedtest-cli', '--json'], stdout=subprocess.PIPE, universal_newlines=True)
+            print('Communicating with subprocess.')
+            s2 = s1.communicate()
+            try:
+                print('Loading speedtest into JSON variable.')
+                st_json = json.loads(s2[0])
+            except json.decoder.JSONDecodeError:
+                print('ERROR: Failed to parse JSON, setting all values to 0')
+                eprint("ERROR: Failed to parse JSON, setting all values to 0!")
+                st_json = {'download': 0, 'upload': 0, 'ping': 0}
+        except TypeError:
+            print("Couldn't get results from speedtest-cli!")
 
+        print('Setting gauge values.')
         download_speed_gauge.set(st_json['download'])
         upload_speed_gauge.set(st_json['upload'])
         latency_speed_gauge.set(st_json['ping'])
